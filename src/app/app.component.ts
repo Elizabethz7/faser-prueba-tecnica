885d290c8c4250589cd683474ba81ca5590c41c5
Elizabethz7 <47807206+Elizabethz7@users.noreply.github.com>
2021-07-06 22:12:57 -0600

    nueva tarea
    creacion de nueva tarea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AppService } from './app.service';
 import { Tarea } from './tarea';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-root',
@@ -9,16 +10,23 @@ import { Tarea } from './tarea';
 })
 export class AppComponent {
 	tareas: Tarea[];
+	nuevaTarea = this.formBuilder.group(new Tarea(0,"",0));
 
 	constructor(
-        public service: AppService,
+		public service: AppService,
+		private formBuilder: FormBuilder,
 	) { }
-	
+
 	ngOnInit() {
 		this.obtenerTareas();
 	}
 
 	async obtenerTareas() {
 		this.tareas = await this.service.obtenerTareas();
+	}
+
+	onSubmit(): void {
+		this.tareas.push(this.nuevaTarea.value)
+		this.nuevaTarea.reset();
 	}
 }
